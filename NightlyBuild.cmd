6ef59c4f12f9091f5831cc6a87e14791323ba718
Andrew Arnott <andrewarnott@gmail.com>
2016-04-24 12:56:00 -0700

    Use Nerdbank.GitVersioning

--- NightlyBuild.cmd
+++ NightlyBuild.cmd
@@ -7,20 +7,16 @@ solution. Note that this also modifies the following file, replacing
 placeholder version numbers with actual version numbers:
     src\common\GlobalAssemblyInfo.cs
 
-(see the SetVersionNumber task in xunit.performance.msbuild)
+(see the SetVersionNumber target in xunit.performance.msbuild)
 
 This file will, therefore, show up as modified after the build.
 Be careful NOT to check it in that way!
 
-If you publish these packages to Nuget, then please remember to
-bump the build number on BuildSemanticVersion below.
 ======================================================================
 
 :main
 setlocal
 
-set BuildAssemblyVersion=1.0.0.30
-set BuildSemanticVersion=1.0.0-alpha-build0030
 set OutputDirectory=%~dp0LocalPackages
 set DotNet=%~dp0\tools\cli\bin\dotnet
 
